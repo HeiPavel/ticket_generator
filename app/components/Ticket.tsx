47e475d2ac8442bcae84318b93696329265cc081
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2025-06-25 16:28:17 +0300

    Add ticket shape

--- app/components/Ticket.tsx
+++ app/components/Ticket.tsx
@@ -2,16 +2,26 @@
 
 import Image from 'next/image'
 import { FormDataType } from './FormData'
+import ticket from '@/public/assets/icons/pattern-ticket.svg'
 
 export function Ticket({data}: {data: FormDataType}) {
   return (
-    <div className='relative size-56 border border-black'>
-      <Image
-        src={data.avatar}
-        alt='User photo'
-        className='object-cover'
-        fill
-      />
+    <div className='w-full flex justify-center'>
+      <div className='relative flex justify-center items-center overflow-hidden max-w-[600px] aspect-[15/7] grow backdrop-blur-xl clip-path-ticket'>
+        <Image
+          src={ticket}
+          alt='ticket image'
+          className='absolute w-full'
+        />
+        <div className='relative size-56'>
+          <Image
+            src={data.avatar}
+            alt='Avatar image'
+            fill
+            className='object-cover'
+          />
+        </div>
+      </div>
     </div>
   )
 }
